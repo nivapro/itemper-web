7b122f856284178ec4f707591a86d8f33d916cb0
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-02-27 08:12:47 +0100

    Add windows.location info

--- src/features/admin/admin-config-card.vue
+++ src/features/admin/admin-config-card.vue
@@ -5,6 +5,10 @@
             <v-card-title>Configuration:</v-card-title>
             <v-card-text><pre>{{config}}</pre></v-card-text>
         </v-card>
+        <v-card>
+            <v-card-title>window.location:</v-card-title>
+            <v-card-text><pre>{{location}}</pre></v-card-text>
+        </v-card>
     </div>
 </template>
 <script lang='ts'>
@@ -15,6 +19,7 @@ import { Vue, Component} from 'vue-property-decorator';
 @Component({})
 export default class ConfigCard extends Vue {
     public config = JSON.stringify(config, undefined, 2);
+    public location = JSON.stringify(window.location, undefined, 2);
 }
 </script>
 <style>
