3ec4cc8b31fdf966926f33a82678c70522dd62a8
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-03-24 16:59:41 +0100

    Build for production locally

---
+++ build-image-production.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Assume Build itemper web front-end
+if [ ! -d dist ]  
+then  
+echo "./dist does not exist"
+exit 1  
+fi 
+# Login to docker hub
+cat  ~/.docker-pwd.txt | docker login --username tova --password-stdin
+
+# Build and tag image
+docker build . --file Dockerfile --tag itemper
+
+version=v$(node -p "require('./package.json').version")
+
+docker tag itemper tova/itemper-web:${version} 
+docker tag itemper tova/itemper-web:latest 
+
+# Push image to Docker hub
+docker push tova/itemper-web:${version} 
+docker push tova/itemper-web:latest
